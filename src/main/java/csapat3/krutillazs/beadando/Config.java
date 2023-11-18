f0b5dbf4ae72b7817277081305e20bbab663602d
Norbert Kecső <flawlesslol123@gmail.com>
2023-11-19 00:23:39 +0100

    Add enum,  Config logic refact

--- src/main/java/csapat3/krutillazs/beadando/Config.java
+++ src/main/java/csapat3/krutillazs/beadando/Config.java
@@ -1,19 +1,19 @@
 package csapat3.krutillazs.beadando;
 
-import csapat3.krutillazs.beadando.Interfaces.IConfig;
+import csapat3.krutillazs.beadando.Enums.ConfigKey;
 import csapat3.krutillazs.beadando.Interfaces.IContainer;
 import csapat3.krutillazs.beadando.Utils.Array;
 import io.github.cdimascio.dotenv.Dotenv;
 
-public final class Config implements IConfig {
+public final class Config {
     public static String get(String config) {
         Dotenv dotenv = Dotenv.load();
         IContainer container = IContainer.getInstance();
 
-        Array Array = container.resolve(Array.class).setArray(IConfig.ACCEPTED_CONFIGS);
+        Array Array = container.resolve(Array.class).setArray(ConfigKey.getAcceptedConfigs());
 
         if (!Array.has(config)) {
-            throw new Error("Config is not supported. Supported list: " + String.join(", ", IConfig.ACCEPTED_CONFIGS));
+            throw new Error("Config is not supported. Supported list: " + String.join(", ", ConfigKey.getAcceptedConfigs()));
         }
 
         return dotenv.get(config);
