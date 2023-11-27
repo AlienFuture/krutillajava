57cfae9ee8b4e527f6894aeb24df9ffa1ace8c75
Balázs Vámos <vamos.balazs@outlook.com>
2023-11-27 16:35:28 +0100

    Log hozzáadás, nem használt eventek törlése

--- src/main/java/csapat3/krutillazs/beadando/Interfaces/LoggerInterface.java
+++
@@ -1,7 +0,0 @@
-package csapat3.krutillazs.beadando.Interfaces;
-
-import csapat3.krutillazs.beadando.Enums.LogType;
-
-public interface LoggerInterface {
-    void log(String message, LogType logType);
-}
